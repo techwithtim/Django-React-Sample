bc097552247d444b058a30a67ffbead0f1315506
Tim Ruscica <techwithtimm@gmail.com>
2024-03-13 13:25:38 +0400

    add loading indicator

--- backend/db-script.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-sleep 10
-python3 manage.py makemigrations
-python3 manage.py migrate
--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -3,13 +3,16 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/FormStyles.css";
+import LoadingIndicator from "../components/LoadingIndicator";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false)
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
+        setLoading(true)
         e.preventDefault();
         try {
             const res = await api.post(route, { username, password });
@@ -22,6 +25,8 @@ function Form({ route, method }) {
             }
         } catch (error) {
             alert(error);
+        } finally {
+            setLoading(false)
         }
     };
 
@@ -42,6 +47,7 @@ function Form({ route, method }) {
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
             />
+            {loading && <LoadingIndicator />}
             <button className="form-button" type="submit">
                 {method === "login" ? "Login" : "Register"}
             </button>
