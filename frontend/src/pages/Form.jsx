5e9ef9068c1e5772db34b450328c65e138b28421
Tim Ruscica <techwithtimm@gmail.com>
2024-03-13 12:29:57 +0400

    remove loading indicator

--- frontend/src/pages/Form.jsx
+++ frontend/src/pages/Form.jsx
@@ -3,17 +3,14 @@ import api from "../api";
 import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/FormStyles.css";
-import LoadingIndicator from "../components/LoadingIndicator";
 
 function Form({ route, method }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-    const [loading, setLoading] = useState(false)
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDetfault();
-        setLoading(true)
         try {
             const res = await api.post(route, { username, password });
             if (method === "login") {
@@ -25,8 +22,6 @@ function Form({ route, method }) {
             }
         } catch (error) {
             alert(error);
-        } finally {
-            setLoading(false)
         }
     };
 
@@ -47,7 +42,6 @@ function Form({ route, method }) {
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Password"
             />
-            {loading && <LoadingIndicator />}
             <button className="form-button" type="submit">
                 {method === "login" ? "Login" : "Register"}
             </button>
